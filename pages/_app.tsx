5fe07b7e3bc58e95112394ca3deb871a33e96d88
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-03-28 15:26:55 +0900

    yarn, <head> 에러 수정중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import MainLayout from '@/components/layouts/MainLayout'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
 import { ReactElement, ReactNode } from 'react'
 import { RecoilRoot } from 'recoil'
 
@@ -12,6 +14,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <MainLayout>
+      <Head>
+        <meta name="description" content="StarBucks Clone Site" />
+        <meta name="keywords" content="StarBucks, Clone, Site" />
+        <meta name="author" content="SpaLand" />
+        <title>StarBucks Clone Site</title>
+      </Head>
         <Component {...pageProps} />
       </MainLayout>
     </RecoilRoot>

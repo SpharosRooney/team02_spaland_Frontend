154fc6341e066ad8672ff17d8faba23985d2e415
DESKTOP-4F42IDV\교육생12 <pakc918@naver.com>
2023-04-06 15:36:15 +0900

    build error

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import CartHeader from '@/components/page/cart/CartHeader'
 import CartFooter from '@/components/page/cart/CartFooter'
-import CartInfo from '@/components/page/cart/CartInfo'
 import CartMenu from '@/components/page/cart/CartMenu'
 import { cartListType } from '@/types/cart/cartListType'
 import axios from 'axios'
@@ -11,6 +10,7 @@ import { userLoginState } from '@/state/user/atom/userLoginState'
 import Swal from 'sweetalert2'
 import { useRouter } from 'next/router'
 import Config from '@/configs/config.export'
+import Image from 'next/image'
 
 export default function Cart() {
 
@@ -20,20 +20,19 @@ export default function Cart() {
 
     const [cartList, setCartList] = useState<cartListType[]>();
 
-    if (!isLogin) {
-        Swal.fire({
-            icon: 'error',
-            text: '로그인이 필요한 서비스 입니다.',
-            customClass: {
-                confirmButton: 'swal-confirm-button'
-            }
-        }).then(
-            res => res.isConfirmed && router.push('/login')
-        )
-        return;
-    }
-
     useEffect(() => {
+        if (!isLogin) {
+            Swal.fire({
+                icon: 'error',
+                text: '로그인이 필요한 서비스 입니다.',
+                customClass: {
+                    confirmButton: 'swal-confirm-button'
+                }
+            }).then(
+                res => res.isConfirmed && router.push('/login')
+            )
+            return;
+        }
         axios.get(`${baseUrl}/api/v1/cart?isDelete=false`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
@@ -42,7 +41,7 @@ export default function Cart() {
             console.log(res)
             setCartList(res.data.data)
         })
-    }, [])
+    }, [accessToken, baseUrl, isLogin, router])
 
     console.log('cartlist', cartList)
 
@@ -62,7 +61,11 @@ export default function Cart() {
                 </>
             ) :
                 <section className="empty-cart">
-                    <img src="https://cdn-icons-png.flaticon.com/512/2838/2838895.png" />
+                    <Image 
+                    src="https://cdn-icons-png.flaticon.com/512/2838/2838895.png"
+                    width={20}
+                    height={20}
+                    alt='cartImage'/>
                     <p>장바구니가 비어있습니다.</p>
                 </section>}
         </>
